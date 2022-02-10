83a88154d910838c9672f2c806ca9f2cb95af15d
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2022-02-10 09:21:05 +0200

    fix style

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
 const FeedBackOptions = ({ options, onLeaveFeedback }) => {
-  return options.map(option => (
-    <button
-      type="button"
-      className={s.FeedBack}
-      key={option}
-      onClick={() => onLeaveFeedback(option)}
-    >
-      {option}
-    </button>
-  ));
+  return (
+    <div className={s.buttons}>
+      {options.map(option => (
+        <button
+          type="button"
+          className={s.FeedBack}
+          key={option}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 FeedBackOptions.protoType = {
